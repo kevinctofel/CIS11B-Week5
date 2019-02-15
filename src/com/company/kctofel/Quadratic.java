c1e42230ef7c861b95f4bf1fce6cb851fd27ba7d
Kevin C Tofel <ktofel3811@students.mc3.edu>
2019-02-15 16:43:51 +0000

    Initial commit

---
+++ src/com/company/kctofel/Quadratic.java
@@ -0,0 +1,27 @@
+package com.company.kctofel;
+
+public class Quadratic {
+
+    private double a, b, c, x, y;
+
+    // default constructor (calling setters)
+
+    // constructor (calling setters)
+
+    // void setters
+
+    // getters
+
+    // boolean isRoot real
+
+    // boolean isRoot complex
+
+    // boolean isDiscriminant negative
+
+    // double firstDerivative (slope)
+
+    // [list of doubles] real root
+
+    // [list of doubles] complex root
+
+} // end Quadratic
---
+++ src/com/company/kctofel/QuadraticDriver.java
@@ -0,0 +1,8 @@
+package com.company.kctofel;
+
+public class QuadraticDriver {
+
+    public static void main(String[] args) {
+
+    }
+} // end QuadraticDriver
