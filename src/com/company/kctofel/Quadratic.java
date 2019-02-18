b5485c6c0cf42e01fed114adee1ac7ae9751a79f
Kevin C Tofel <ktofel3811@students.mc3.edu>
2019-02-18 22:07:27 +0000

    Roots calculation working

--- src/com/company/kctofel/Quadratic.java
+++ src/com/company/kctofel/Quadratic.java
@@ -1,43 +1,39 @@
 package com.company.kctofel;
 
+import org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
+
 public class Quadratic {
 
-    private double a, b, c, root1, root2;
-    private double[] roots new double[]
+    private double a, b, c, derivative;
+    private double[] roots = new double[2];
 
     // default constructor (calling setters)
     public Quadratic (){
 
-        setVariables(1.0,1.0,0.0);
+        setVariables(1.0,-1.0,0.0);
     }
 
     // constructor (calling setters)
     public Quadratic(double a, double b, double  c) {
-
         setVariables(a,b,c);
     }
 
     // void setters
     public void setVariables(double a, double b, double c){
-
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
-
     public double getA() {
-
         return this.a;
     }
 
     public double getB() {
-
         return this.b;
     }
 
     public double getC() {
-
         return this.c;
     }
 
@@ -56,15 +52,38 @@ public class Quadratic {
 
     // double getDiscriminant
     public double getDiscriminant() {
-
         return Math.pow((this.b), 2.0) - (4 * this.a * this.c);
     }
 
     // double firstDerivative (slope)
+    public double getDerivative() {
+
+        derivative = 2*this.roots[0] + this.b;
+
+        return derivative;
+    }
 
     // [list of doubles] real root
-    public getRoots();
+    public void getRoots() {
+
+        if (!(isDiscriminantNeg())) { // Positive discriminant
+            roots[0] = (-(this.b) + Math.sqrt(getDiscriminant())) / (2 * this.a);
+            roots[1] = (-(this.b) - Math.sqrt(getDiscriminant())) / (2 * this.a);
+        }
+        else {
+            roots[0] = (-(this.b) + Math.sqrt(-(getDiscriminant()))) / (2 * this.a);
+            roots[1] = (-(this.b) - Math.sqrt(-(getDiscriminant()))) / (2 * this.a);
+        }
+    } // end getRoots
+
 
     // [list of doubles] complex root
+
+    // String toString
+
+    public String toString() {
+
+        return ("Root 1: " + roots[0] + "\nRoot 2: " + roots[1] + "\nDerivative: " + derivative);
+    }
 
 } // end Quadratic
--- src/com/company/kctofel/QuadraticDriver.java
+++ src/com/company/kctofel/QuadraticDriver.java
@@ -4,11 +4,19 @@ public class QuadraticDriver {
 
     public static void main(String[] args) {
 
-    Quadratic equation1 = new Quadratic(2, -6, 1);
+    Quadratic equation1 = new Quadratic(2, 4, 1);
     Quadratic equation2 = new Quadratic();
 
     System.out.println(equation1.getDiscriminant());
     System.out.println(equation2.getDiscriminant());
+
+    equation1.getRoots();
+    equation2.getRoots();
+
+    System.out.println(equation1.toString());
+    System.out.println(equation2.toString());
+
+
 
     }
 } // end QuadraticDriver
